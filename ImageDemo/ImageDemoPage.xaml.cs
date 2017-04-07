9598ae3b22bb885a885f919eb7c627db62e19123
smoming <smoming1031@gmail.com>
2017-04-08 00:52:31 +0800

    Image_Example
    Image Example

---
+++ ImageDemo/ImageDemoPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace ImageDemo
+{
+	public partial class ImageDemoPage : ContentPage
+	{
+		public ImageDemoPage()
+		{
+			InitializeComponent();
+
+			xImage.Source = ImageSource.FromResource("ImageDemo.didi.jpg");
+			//xImage.Source = new UriImageSource()
+			//{
+			//	Uri = new Uri("https://goo.gl/8eUKFy"),
+			//	CachingEnabled = true,
+			//	CacheValidity = new TimeSpan(5,0,0,0)
+			//};
+			xImage.Aspect = Aspect.AspectFit;
+		}
+	}
+}
